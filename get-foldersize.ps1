ebfb15fe43a3e292f7b4b31a050f00329ffa1762
Binu Balan <77137647+BinuAppu@users.noreply.github.com>
2024-07-27 16:13:40 +0530

    Add files via upload

---
+++ get-foldersize.ps1
@@ -0,0 +1,52 @@
+cls
+$host.Ui.RawUI.WindowTitle = "Get FolderSize, Created by: Binu Balan"
+Write-Host "=========================================================" -ForegroundColor Cyan
+Write-Host " "
+Write-host "                 _    ____  ____  _   _ "
+Write-host "                / \  |  _ \|  _ \| | | |"
+Write-host "               / _ \ | |_) | |_) | | | |"
+Write-host "              / ___ \|  __/|  __/| |_| |"
+Write-host "             /_/   \_\_|   |_|    \___/ "
+Write-Host " "
+Write-Host "=========================================================" -ForegroundColor Cyan
+Write-Host " "
+Write-host "                 .__." -ForegroundColor Green
+Write-host "                   (oo)____" -ForegroundColor Green
+Write-host "                   (__)    )\" -ForegroundColor Green
+Write-host "                      ll--ll '" -ForegroundColor Green
+Write-Host "               SCRIPT BY BINU BALAN               " -ForegroundColor DarkRed -BackgroundColor White
+Write-Host "<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>" -ForegroundColor Cyan
+Write-Host " "
+Start-Sleep -Seconds 2
+$ErrorActionPreference='SilentlyContinue'
+
+$query = Read-Host("Enter the Folder Path ")
+$SizeQuery = Read-Host("Enter the Output size [G = GB & M = MB ; Default GB]")
+if($SizeQuery -eq "G"){
+    $SizeType = "1GB"
+    $type = "GB"
+
+} elseif($SizeQuery -eq "M"){
+    $SizeType = "1MB"
+    $type = "MB"
+} else {
+    $SizeType = "1GB"
+    $type = "GB"
+}
+$alldata = Get-ChildItem $query -force
+$count = $alldata.count
+$objectSchema = @()
+$i = 0
+
+foreach($data in $alldata){
+    $size = (Get-ChildItem $data.FullName -Recurse -force | Measure-Object -Property Length -Sum).sum
+    $sizeval = [math]::Round($size / $SizeType, 2)
+    $mode = $data.mode
+    $name = $data.Name
+    # Write-Host "$sizeinmb | $mode | $name "
+    $objectSchema += [PSCustomObject]@{Name = $name; mode= $mode; size="$sizeval $type"}
+    $i++
+    Write-Progress -PercentComplete ($i/$count*100) -Status "Processing Items" -Activity "Please Wait !!"
+}
+$ErrorActionPreference='Continue'
+$objectSchema
